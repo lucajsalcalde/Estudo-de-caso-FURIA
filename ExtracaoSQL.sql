d3eb3c6af75927c9f10a6bd946e5115765e3d992
Luca Jordão Salas Alcalde <44827249+lucajsalcalde@users.noreply.github.com>
2022-07-23 23:12:35 -0300

    Update ExtracaoSQL.sql

--- ExtracaoSQL.sql
+++ ExtracaoSQL.sql
@@ -1,6 +1,5 @@
 -- Databricks notebook source
 --Visualizando tabela
---OBS: O jogador drop não estava na tabela, até que o insert fosse executado.
 select *
 from tb_furia
 
@@ -13,7 +12,7 @@ where Player = 'yuurih'
 
 -- COMMAND ----------
 
---Inserindo jogador necessário para análise, que nao estava na tabela após a extração do site
+--Inserindo jogador necessário para análise que nao estava na tabela após a extração do site
 insert into tb_furia (id, Player, Times, Mapas, Rodadas, KdDiff, KD, Desempenho)
 Values ('1000','drop', '', '178', '4654', '-92', '0.97', '0.99')
 
